6977ca0dfe7e0e46db814f42ff21fe2393dc97ab
Nikolaus Georgi <civ.nxgeo@gmail.com>
2023-11-28 12:26:15 +0700

    Upload Project

---
+++ src/springHibernate/koneksi.java
@@ -0,0 +1,24 @@
+package springHibernate;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class koneksi {
+    static Connection c;
+    
+    public static Connection connection(){
+        if(c == null){
+            MysqlDataSource data = new MysqlDataSource();
+            data.setDatabaseName("mhs");
+            data.setUser("root");
+            data.setPassword("");
+            try{
+                c = data.getConnection();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+        return c;
+    }
+}
